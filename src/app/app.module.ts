a375340e7076de4e342f765a2fb1205443ac8d02
Tabata Souto <tabatac.souto@gmail.com>
2022-09-13 20:47:30 -0300

    feat: criação pasta de páginas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { AlbumComponent } from './pages/album/album.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProfileEditComponent } from './pages/profile-edit/profile-edit.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    AlbumComponent,
+    FavoritesComponent,
+    ProfileComponent,
+    ProfileEditComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
