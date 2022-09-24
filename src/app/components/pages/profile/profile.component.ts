13f67d8fcb65edef974a4e0884bd0dd1309303bb
Tabata Souto <tabatac.souto@gmail.com>
2022-09-24 12:48:23 -0300

    feat: efeito de loading enquanto a página profile carrega.

--- src/app/components/pages/profile/profile.component.ts
+++ src/app/components/pages/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
 import { ILogin } from 'src/app/interfaces/ILogin';
 import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
  
@@ -12,14 +14,21 @@ import { LocalStorageService } from 'src/app/services/local-storage/local-storag
 
 export class ProfileComponent implements OnInit {
   user!: ILogin;
+  isLoading: boolean = false;
+
   faUserCircle = faUserCircle;
+  faSpinner = faSpinner;
   
   constructor(
     private localStorageService: LocalStorageService
   ) { }
 
   ngOnInit(): void {
-    this.user = this.localStorageService.get('user');
+    this.isLoading = true;
+    setTimeout(() => {
+      this.user = this.localStorageService.get('user');
+      this.isLoading = false;
+    }, 1000)
   }
 
 }
