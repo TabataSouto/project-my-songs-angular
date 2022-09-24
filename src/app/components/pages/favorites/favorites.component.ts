ff9badac17df6ab79cb5f63500f90055cfb80e14
Tabata Souto <tabatac.souto@gmail.com>
2022-09-24 01:14:51 -0300

    feat: renderização das músicas favoritadas, também com a opção de remoção.

--- src/app/components/pages/favorites/favorites.component.ts
+++ src/app/components/pages/favorites/favorites.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ISong } from 'src/app/interfaces/ISong';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
+
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css']
 })
-export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+export class FavoritesComponent implements OnInit {
+  isLoading: boolean = false;
+  favoriteSongs: ISong[] = [];
+  faSpinner = faSpinner;
+
+  constructor(
+    private localStorageService: LocalStorageService
+  ) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.favoriteSongs = this.localStorageService.get('favorites');
+      this.isLoading = false;
+    }, 1000);
   }
 
 }
