93f423701974e08eb2111de10f07b6a4b5fcee45
Tabata Souto <tabatac.souto@gmail.com>
2022-09-24 16:45:49 -0300

    feat: foto do usuário no header.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
 
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   username!: string;
+  imageProfile!: string;
+  faUserCircle = faUserCircle;
 
   constructor(
     private localStorageService: LocalStorageService
@@ -16,6 +21,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     const data = this.localStorageService.get('user');
     this.username = data.username;
+    this.imageProfile = data.image;
   }
 
 }
